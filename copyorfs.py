0e4481a713fe60a1b6bc03c37d1b6f09a1d215d0
Witek Firlej <git@grizz.pl>
2009-04-16 18:23:04 +0200

    initial; script works but it isn't foolproof ;)

---
+++ copyorfs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# copy orf files which corresponding jpg ones by Witek Firlej http://grizz.pl
+
+__project__      = "copyorfs"
+__author__    = "Witold Firlej (http://grizz.pl)"
+__license__   = "GPL"
+__copyright__ = "Witold Firlej"
+__version__ = "0.3"
+
+import glob,os
+print os.path.realpath(os.curdir)
+print os.curdir
+for infile in glob.glob("*.JPG"):
+	print "copying: "+infile[:-4]+".ORF" 			# [:-4] cutting extension
+	os.system("cp ../"+infile[:-4]+".ORF .")
+print "\nDone!\n"
