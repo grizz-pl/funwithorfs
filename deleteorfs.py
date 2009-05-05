7db231b5a3e3c4cf1546c9ae21a5dbdf8a709c5d
Witek Firlej <git@grizz.pl>
2009-05-05 11:20:09 +0200

    deleteorfs.py - delete orf files without corresponding jpgs Please enter the commit message for your changes. Lines starting

---
+++ deleteorfs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# delete orf files without corresponding jpgs by Witek Firlej http://grizz.pl
+
+__project__      = "deleteorfs"
+__author__    = "Witold Firlej (http://grizz.pl)"
+__license__   = "GPL"
+__copyright__ = "Witold Firlej"
+__version__ = "0.1"
+
+import glob,os
+print os.path.realpath(os.curdir)
+print os.curdir
+os.system("mkdir todelete") 						# being cerful is very important ;)
+for infile in glob.glob("*.ORF"):
+	if not os.path.isfile(infile[:-4]+".JPG"):
+		print "deleting: "+infile[:-4]+".ORF" 			# [:-4] cutting extension
+		os.system("mv "+infile[:-4]+".ORF todelete/")
+print "\nDone!\n"
