60e14e4c0009c6021a61b4474574b7c544e77fbb
Witek Firlej <git@grizz.pl>
2010-03-04 00:22:55 +0100

    affect both *.jpg and *.JPG files

--- copyorfs.py
+++ copyorfs.py
@@ -6,12 +6,12 @@ __project__      = "copyorfs"
 __author__    = "Witold Firlej (http://grizz.pl)"
 __license__   = "GPL"
 __copyright__ = "Witold Firlej"
-__version__ = "0.3"
+__version__ = "0.35"
 
 import glob,os
 print os.path.realpath(os.curdir)
 print os.curdir
-for infile in glob.glob("*.JPG"):
+for infile in glob.glob("*.[jJ][pP][gG]"):
 	print "copying: "+infile[:-4]+".ORF" 			# [:-4] cutting extension
 	os.system("cp ../"+infile[:-4]+".ORF .")
 print "\nDone!\n"
--- deleteorfs.py
+++ deleteorfs.py
@@ -6,14 +6,14 @@ __project__      = "deleteorfs"
 __author__    = "Witold Firlej (http://grizz.pl)"
 __license__   = "GPL"
 __copyright__ = "Witold Firlej"
-__version__ = "0.1"
+__version__ = "0.15"
 
 import glob,os
 print os.path.realpath(os.curdir)
 print os.curdir
 os.system("mkdir todelete") 						# being cerful is very important ;)
-for infile in glob.glob("*.ORF"):
-	if not os.path.isfile(infile[:-4]+".JPG"):
+for infile in glob.glob("*.[oO][rR][fF]"):
+	if not os.path.isfile(infile[:-4]+".[jJ][pP][gG]"):
 		print "deleting: "+infile[:-4]+".ORF" 			# [:-4] cutting extension
 		os.system("mv "+infile[:-4]+".ORF todelete/")
 print "\nDone!\n"
